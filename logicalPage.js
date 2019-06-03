065a3f2c4cebdcab3f84aeef3494e7f652b344d3
Penn <penndeng@foxmail.com>
2019-06-03 13:29:31 +0800

    new resository

---
+++ logicalPage.js
@@ -0,0 +1,21 @@
+
+function password() {   
+    var testV = 1;   
+    var pass1 = prompt('请输入密码','');   
+    while (testV < 3000000) {   
+    if (!pass1)   
+    history.go(-1);   
+    if (pass1 == "g2d)l4^8") { 
+    // alert('密码正确!');   
+    break;   
+    }   
+    testV+=1;   
+    var pass1 =   
+    prompt('密码错误!请重新输入:');   
+    }   
+    if (pass1!="password" & testV == 3000000)   
+    history.go(-1);   
+    return " ";   
+    }   
+
+document.write(password());  
